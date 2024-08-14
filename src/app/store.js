a7809957dcbc05ce3a26fe46f2476cffa9b85bda
trungluongdev <chitrungluong.cv@gmail.com>
2024-08-14 12:26:02 +0700

    initialize project

---
+++ src/app/store.js
@@ -0,0 +1,60 @@
+// import { createStore, combineReducers, applyMiddleware } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import counterReducer from "../features/counter/reducer";
+// import multiCounterReducer from "../features/multiCounter/reducer";
+// import todoReducer from "../features/todo/reducer";
+// import { thunk } from "redux-thunk";
+
+// const initialState = {};
+// const store = createStore(
+//   combineReducers({
+//     counter: counterReducer,
+//     multiCounter: multiCounterReducer,
+//     todo: todoReducer,
+//   }),
+//   initialState,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+// export default store;
+
+// import { configureStore } from '@reduxjs/toolkit'
+// import counterReducer from "../features/counter/reducer";
+// import multiCounterReducer from "../features/multiCounter/reducer";
+// import todoReducer from "../features/todo/reducer";
+
+
+// const store = configureStore({
+//   reducer: {
+//     counter: counterReducer,
+//     multiCounter: multiCounterReducer,
+//     todo: todoReducer,
+//   },
+// })
+
+// export default store
+
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk"; // Import redux-thunk
+import counterReducer from "../features/counter/reducer";
+import multiCounterReducer from "../features/multiCounter/reducer";
+import todoReducer from "../features/todo/reducer";
+
+// Combine reducers
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  multiCounter: multiCounterReducer,
+  todo: todoReducer,
+});
+
+const initialState = {};
+
+// Create store with only redux-thunk middleware
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(thunk)
+);
+
+export default store;
+
